0700ca653fc8f15c3a924e951666c9a15e9227c2
Shriji Thakker <shrijithakker@Shrijis-MacBook-Pro.local>
2024-12-19 20:47:42 -0800

    Pricing Front end started

--- app/page.js
+++ app/page.js
@@ -6,9 +6,10 @@ export default function Home() {
 
   return (
     <main>
+      {/* Header Section */}
       <section className="bg-base-200">
         <div className="flex space-x-3 justify-between items-center px-8 py-2 max-w-3xl mx-auto">
-          <div className="font-bold">Shrobro.IO</div>
+          <div className="font-black uppercase">Shrobro.IO</div>
           <div className="space-x-4 max-md:hidden">
             <a className="link link-primary">Pricing</a>
             <a className="link link-primary">FAQ</a>
@@ -18,6 +19,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+      {/* Hero Section */}
       <section className=" py-32 px-8 text-center max-w-3xl mx-auto">
         <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
           Collect customer feedback to build better products.
@@ -27,6 +29,83 @@ export default function Home() {
           products your customer will love.
         </div>
         <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+
+      {/* Pricing Section */}
+      <section className="bg-base-200">
+        <div className=" py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary">
+            Pricing
+          </p>
+          <h2 className=" text-center text-3xl lg:text-4xl font-extrabold mb-1">
+            One price: $49.99
+          </h2>
+          <div className="text-center opacity-90 mb-12">
+            Pay once, unlimited access forever.
+          </div>
+          <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto space-y-6">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">$50</div>
+              <div className="uppercase text-sm font-medium opacity-60">
+                /MONTH
+              </div>
+            </div>
+            <ul className="space-y-2">
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="text-primary size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
+                Collect customer feedback
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="text-primary size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
+                Admin dashboard
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="text-primary size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
+                24/7 support
+              </li>
+            </ul>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          </div>
+        </div>
       </section>
     </main>
   );
